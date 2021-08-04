364dbc2145733d12118584b40e782aa057b92de8
barebonesapps <74522982+barebonesapps@users.noreply.github.com>
2021-08-04 14:09:06 -0400

    Set up bet types and descriptions

--- UpBets/Form1.Designer.cs
+++ UpBets/Form1.Designer.cs
@@ -33,6 +33,8 @@ namespace UpBets
             this.txtBankroll = new System.Windows.Forms.TextBox();
             this.lblBetType = new System.Windows.Forms.Label();
             this.ddlBetType = new System.Windows.Forms.ComboBox();
+            this.txtBetDest = new System.Windows.Forms.TextBox();
+            this.btnCalcUpBets = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblBankroll
@@ -47,9 +49,9 @@ namespace UpBets
             // 
             // txtBankroll
             // 
-            this.txtBankroll.Location = new System.Drawing.Point(122, 10);
+            this.txtBankroll.Location = new System.Drawing.Point(105, 10);
             this.txtBankroll.Name = "txtBankroll";
-            this.txtBankroll.Size = new System.Drawing.Size(51, 23);
+            this.txtBankroll.Size = new System.Drawing.Size(68, 23);
             this.txtBankroll.TabIndex = 1;
             this.txtBankroll.Text = "500";
             this.txtBankroll.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -58,7 +60,7 @@ namespace UpBets
             // 
             this.lblBetType.AutoSize = true;
             this.lblBetType.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblBetType.Location = new System.Drawing.Point(12, 59);
+            this.lblBetType.Location = new System.Drawing.Point(221, 8);
             this.lblBetType.Name = "lblBetType";
             this.lblBetType.Size = new System.Drawing.Size(88, 25);
             this.lblBetType.TabIndex = 2;
@@ -66,18 +68,41 @@ namespace UpBets
             // 
             // ddlBetType
             // 
-            this.ddlBetType.Enabled = false;
             this.ddlBetType.FormattingEnabled = true;
-            this.ddlBetType.Location = new System.Drawing.Point(122, 60);
+            this.ddlBetType.Location = new System.Drawing.Point(326, 14);
             this.ddlBetType.Name = "ddlBetType";
             this.ddlBetType.Size = new System.Drawing.Size(198, 23);
             this.ddlBetType.TabIndex = 3;
+            this.ddlBetType.SelectedIndexChanged += new System.EventHandler(this.ddlBetType_SelectedIndexChanged);
+            // 
+            // txtBetDest
+            // 
+            this.txtBetDest.Enabled = false;
+            this.txtBetDest.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtBetDest.Location = new System.Drawing.Point(12, 55);
+            this.txtBetDest.Multiline = true;
+            this.txtBetDest.Name = "txtBetDest";
+            this.txtBetDest.Size = new System.Drawing.Size(512, 139);
+            this.txtBetDest.TabIndex = 4;
+            // 
+            // btnCalcUpBets
+            // 
+            this.btnCalcUpBets.BackColor = System.Drawing.Color.White;
+            this.btnCalcUpBets.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcUpBets.Location = new System.Drawing.Point(178, 221);
+            this.btnCalcUpBets.Name = "btnCalcUpBets";
+            this.btnCalcUpBets.Size = new System.Drawing.Size(193, 39);
+            this.btnCalcUpBets.TabIndex = 5;
+            this.btnCalcUpBets.Text = "&Calculate";
+            this.btnCalcUpBets.UseVisualStyleBackColor = false;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(512, 617);
+            this.ClientSize = new System.Drawing.Size(549, 617);
+            this.Controls.Add(this.btnCalcUpBets);
+            this.Controls.Add(this.txtBetDest);
             this.Controls.Add(this.ddlBetType);
             this.Controls.Add(this.lblBetType);
             this.Controls.Add(this.txtBankroll);
@@ -96,6 +121,8 @@ namespace UpBets
         private System.Windows.Forms.TextBox txtBankroll;
         private System.Windows.Forms.Label lblBetType;
         private System.Windows.Forms.ComboBox ddlBetType;
+        private System.Windows.Forms.TextBox txtBetDest;
+        private System.Windows.Forms.Button btnCalcUpBets;
     }
 }
 
--- UpBets/Form1.cs
+++ UpBets/Form1.cs
@@ -20,13 +20,65 @@ namespace UpBets
         private void frmMain_Load(object sender, EventArgs e)
         {
             FillDDList();
+            //Set the bet type to red/black
+            ddlBetType.SelectedIndex = 0;
         }
 
         private void FillDDList()
         {
             //DropDownList1.Items.Add(new ListItem("All Record"));
+            ddlBetType.Items.Add("Red/Black");
+            ddlBetType.Items.Add("Low/High");
+            ddlBetType.Items.Add("Even/Odd");
+            ddlBetType.Items.Add("Dozen");
+            ddlBetType.Items.Add("Column");
+            ddlBetType.Items.Add("Double Street");            
+            ddlBetType.Items.Add("Corner");
+            ddlBetType.Items.Add("Street");
+            ddlBetType.Items.Add("Split");
+            ddlBetType.Items.Add("Straight-Up");
+        }
 
-            
+        private void ddlBetType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(ddlBetType.SelectedItem != null)
+            {
+                string selectedItem = ddlBetType.SelectedItem.ToString();
+                //MessageBox.Show(selectedItem, "y", MessageBoxButtons.OK);
+                switch (selectedItem)
+                {
+                    case "Red/Black":
+                        txtBetDest.Text = string.Format("Red/Black{0}Place bet on either red or black{0}Payout 1:1{0}American Roulette Odds 47.40%{0}European Roulette Odds 48.60%", Environment.NewLine);
+                        break;
+                    case "Low/High":
+                        txtBetDest.Text = string.Format("Low/High{0}Any number from 1-18 / Any number from 19-36{0}Payout 1:1{0}American Roulette Odds 47.40%{0}European Roulette Odds 48.60%", Environment.NewLine);
+                        break;
+                    case "Even/Odd":
+                        txtBetDest.Text = string.Format("Even/Odd{0}Any even numbers / Any odd numbers{0}Payout 1:1{0}American Roulette Odds 47.40%{0}European Roulette Odds 48.60%", Environment.NewLine);
+                        break;
+                    case "Dozen":
+                        txtBetDest.Text = string.Format("Dozen{0}Payout 2:1{0}The first dozen (1-12) second dozen (13-24) or third dozen (25-36){0}American Roulette Odds 31.60%{0}European Roulette Odds 32.40%", Environment.NewLine);
+                        break;
+                    case "Column":
+                        txtBetDest.Text = string.Format("Column{0}Payout 2:1{0}One of the three rows of numbers on the roulette table (3-36), (2-35), (1-34){0}American Roulette Odds 31.60%{0}European Roulette Odds 32.40%", Environment.NewLine);
+                        break;
+                    case "Double Street":
+                        txtBetDest.Text = string.Format("Double Street{0}Payout 5:1{0}Any six numbers that create two lines (i.e., 1, 2, 3, 4, 5, 6){0}American Roulette Odds 15.80%{0}European Roulette Odds 16.20%", Environment.NewLine);
+                        break;
+                    case "Corner":
+                        txtBetDest.Text = string.Format("Corner{0}Payout 8:1{0}Four numbers in a square layout (i.e., 13, 14, 16, 17){0}American Roulette Odds 10.50%{0}European Roulette Odds 10.80%", Environment.NewLine);
+                        break;
+                    case "Street":
+                        txtBetDest.Text = string.Format("Street{0}Payout 11:1{0}Any three numbers in a line (i.e., 21, 20, 19){0}American Roulette Odds 7.90%{0}European Roulette Odds 8.10%", Environment.NewLine);
+                        break;
+                    case "Split":
+                        txtBetDest.Text = string.Format("Split{0}Payout 17:1{0}Any two numbers (i.e., 7 and 12){0}American Roulette Odds 5.30%{0}European Roulette Odds 5.40%", Environment.NewLine);
+                        break;
+                    case "Straight-Up":
+                        txtBetDest.Text = string.Format("Straight-Up{0}Any single number (i.e., 8 or 15){0}Payout 35:1{0}American Roulette Odds 2.60%{0}European Roulette Odds 2.70%", Environment.NewLine);
+                        break;
+                }
+            }
         }
     }
 }
